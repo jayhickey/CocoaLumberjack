afda65c3247e691b0d411c1a173fa396f712f90b
jayhickey <johnrhickey@gmail.com>
2015-07-25 22:52:27 -0400

    Fixed type in framework name

---
+++ Cocoalumberjack-watchOS/CocoaLumberjack-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Cocoalumberjack-watchOS.h
+//  Cocoalumberjack-watchOS
+//
+//  Created by johnrhickey on 7/25/15.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Cocoalumberjack-watchOS.
+FOUNDATION_EXPORT double CocoaLumberjack_watchOSVersionNumber;
+
+//! Project version string for Cocoalumberjack-watchOS.
+FOUNDATION_EXPORT const unsigned char CocoaLumberjack_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaLumberjack_watchOS/PublicHeader.h>
+
+
